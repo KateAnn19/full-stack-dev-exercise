5c83413164db7fd83a436a795715df63a863f72b
kate mcgee <mcgeehee@gmail.com>
2020-10-12 15:14:08 -0500

    added loader icon

--- annelewis/src/App.js
+++ annelewis/src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./assets/css/style.css";
+
+import UserCards from './userCards.js';
 
 import Head from "./Head";
 import Body from "./Body";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  function toggleButton(){
+    setToggle(!toggle);
+  }
   return (
-    <>
-    <table>
-      <Head />
-      <Body />
-    </table>
-    <div>
-
+    <div className="App">
+      <table>
+        <Head />
+        <Body />
+      </table>
+      <div>
+        <button onClick={toggleButton} className="user_card_button">Click For User Cards</button>
+        {toggle ? <UserCards/> : null} 
+      </div>
     </div>
-    </>
   );
 }
 
--- annelewis/src/userCards.js
+++ annelewis/src/userCards.js
@@ -1,5 +1,64 @@
-axios.get("http://faker.hook.io/?property=helpers.userCard&amp;locale=en_US")
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => console.error(error));
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//styles
+import "./UserCards.css";
+
+function UserCards() {
+  const [userData, setUserData] = useState([]);
+  const [promiseComplete, setPromiseComplete] = useState(false);
+  const dataLimit = 25;
+  const data = [];
+
+  useEffect(() => {
+    makeApiCalls();
+  }, []);
+
+  function makeApiCalls() {
+    for (let i = 0; i < dataLimit; i++) {
+      axios
+        .get("http://faker.hook.io/?property=helpers.userCard&amp;locale=en_US")
+        .then((response) => {
+          data.push(response.data);
+        })
+        .catch((error) => console.error(error));
+    }
+
+    setTimeout(function () {
+      setUserData(data);
+      return setPromiseComplete(!promiseComplete);
+    }, 9000);
+  }
+
+  return (
+    <div className="container">
+      {promiseComplete && userData.length >= 2 ? (
+        userData.map((u) => (
+          <div className="card" key={u.name}>
+            <h2>{u.name}</h2>
+            <h2>{u.username}</h2>
+            <h2>{u.email}</h2>
+            <h2>{u.address.street}</h2>
+            <h2>{u.address.suite}</h2>
+            <h2>{u.address.city}</h2>
+            <h2>{u.address.zipcode}</h2>
+            <h2>{u.company.name}</h2>
+            <h2>{u.company.catchPhrase}</h2>
+            <h2>{u.company.bs}</h2>
+            <h2>{u.phone}</h2>
+            <h2>{u.website}</h2>
+          </div>
+        ))
+      ) : (
+        <>
+          <div className="loader5"></div>
+          <div>
+            <h2>Data Loading...This is will take up to 1 minute...</h2>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default UserCards;
